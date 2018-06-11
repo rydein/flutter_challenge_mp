7ce19294583fef765666434164cab51fd438ce74
rydein <daisuke2ueta@gmail.com>
2018-06-12 00:36:45 +0900

    add radial seek bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_challenge_mp/bottom_controls.dart';
 import 'package:flutter_challenge_mp/songs.dart';
 import 'package:flutter_challenge_mp/theme.dart';
+import 'package:meta/meta.dart';
 
 void main() => runApp(new MyApp());
 
@@ -63,10 +64,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: new Container(
               width: 125.0,
               height: 125.0,
-              child: new ClipOval(
-                clipper: new CircleClipper(),
-                child: new Image.network(demoPlaylist.songs[0].albumArtUrl,
-                    fit: BoxFit.cover),
+              child: new RadialSeekBar(
+                progressPercent: 0.2,
+                thumbPosition: 0.2,
+                child: new ClipOval(
+                  clipper: new CircleClipper(),
+                  child: new Image.network(demoPlaylist.songs[0].albumArtUrl,
+                      fit: BoxFit.cover),
+                ),
               ),
             )),
           ),
@@ -85,3 +90,126 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class RadialSeekBar extends StatefulWidget {
+  final double trackWidth;
+  final Color trackColor;
+  final double progressWidth;
+  final Color progressColor;
+  final double progressPercent;
+  final double thumbSize;
+  final Color thumbColor;
+  final double thumbPosition;
+  final Widget child;
+
+  RadialSeekBar({
+    this.trackWidth = 3.0,
+    this.trackColor = Colors.grey,
+    this.progressWidth = 5.0,
+    this.progressColor = Colors.black,
+    this.progressPercent = 0.0,
+    this.thumbSize = 10.0,
+    this.thumbColor = Colors.black,
+    this.thumbPosition = 0.0,
+    this.child,
+  });
+  @override
+  _RadialSeekBarState createState() => _RadialSeekBarState();
+}
+
+class _RadialSeekBarState extends State<RadialSeekBar> {
+  @override
+  Widget build(BuildContext context) {
+    return new CustomPaint(
+      painter: new RadialSeekBarPainter(
+        trackWidth: widget.trackWidth,
+        trackColor: widget.trackColor,
+        progressWidth: widget.progressWidth,
+        progressColor: widget.progressColor,
+        progressPercent: widget.progressPercent,
+        thumbSize: widget.thumbSize,
+        thumbColor: widget.thumbColor,
+        thumbPosition: widget.thumbPosition,
+      ),
+      child: widget.child,
+    );
+  }
+}
+
+class RadialSeekBarPainter extends CustomPainter {
+  final double trackWidth;
+  final Color trackColor;
+  final Paint trackPaint;
+  final double progressWidth;
+  final Color progressColor;
+  final Paint progressPaint;
+  final double progressPercent;
+  final double thumbSize;
+  final Color thumbColor;
+  final Paint thumbPaint;
+  final double thumbPosition;
+
+  RadialSeekBarPainter({
+    @required this.trackWidth,
+    @required trackColor,
+    @required this.progressWidth,
+    @required progressColor,
+    @required this.progressPercent,
+    @required this.thumbSize,
+    @required thumbColor,
+    @required this.thumbPosition,
+  })  : trackPaint = new Paint()
+          ..color = trackColor
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = trackWidth,
+        progressPaint = new Paint()
+          ..color = progressColor
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = progressWidth
+          ..strokeCap = StrokeCap.round,
+        thumbPaint = new Paint()
+          ..color = thumbColor
+          ..style = PaintingStyle.fill;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = new Offset(size.width / 2, size.height / 2);
+    final radius = min(size.width, size.height) / 2;
+    // paint track.
+    canvas.drawCircle(
+      center,
+      radius,
+      trackPaint,
+    );
+
+    // paint progress.
+    final progressAngle = 2 * pi * progressPercent;
+
+    canvas.drawArc(
+      new Rect.fromCircle(
+        center: center,
+        radius: radius,
+      ),
+      -pi / 2,
+      progressAngle,
+      false,
+      progressPaint,
+    );
+
+    // paint thumb.
+    final thumbAngle = 2 * pi * thumbPosition - (pi / 2);
+    final thumbX = cos(thumbAngle) * radius;
+    final thumbY = sin(thumbAngle) * radius;
+    final thumbCenter = new Offset(thumbX, thumbY) + center;
+    final thumbRadius = thumbSize / 2.0;
+    canvas.drawCircle(
+      thumbCenter,
+      thumbRadius,
+      thumbPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
