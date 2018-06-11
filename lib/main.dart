eee2ba177c057817a630730ceee327dde7427d20
rydein <daisuke2ueta@gmail.com>
2018-06-11 23:30:23 +0900

    add effect to buttons

--- lib/main.dart
+++ lib/main.dart
@@ -78,86 +78,94 @@ class _MyHomePageState extends State<MyHomePage> {
 
           // Song title, artist name, and controls
           new Container(
-              color: accentColor,
-              child: new Padding(
-                padding: const EdgeInsets.only(top: 40.0, bottom: 50.0),
-                child: new Column(
-                  children: <Widget>[
-                    new RichText(
-                        text: new TextSpan(
-                      text: '',
-                      children: [
-                        new TextSpan(
-                          text: 'Song Title\n',
-                          style: new TextStyle(
-                            color: Colors.white,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 4.0,
-                            height: 1.5,
-                          ),
-                        ),
-                        new TextSpan(
-                          text: 'Artist Name',
-                          style: new TextStyle(
-                            color: Colors.white.withOpacity(0.75),
-                            fontSize: 12.0,
-                            letterSpacing: 3.0,
-                            height: 1.5,
-                          ),
-                        )
-                      ],
-                    )),
-                    new Padding(
-                        padding: const EdgeInsets.only(top: 40.0),
-                        child: new Row(
-                          children: <Widget>[
-                            new Expanded(child: new Container()),
-                            new IconButton(
-                              icon: new Icon(
-                                Icons.skip_previous,
-                                color: Colors.white,
-                                size: 35.0,
-                              ),
-                              onPressed: () {
-                                // TODO
-                              },
+              width: double.infinity,
+              child: new Material(
+                shadowColor: const Color(0x44000000),
+                color: accentColor,
+                child: new Padding(
+                  padding: const EdgeInsets.only(top: 40.0, bottom: 50.0),
+                  child: new Column(
+                    children: <Widget>[
+                      new RichText(
+                          text: new TextSpan(
+                        text: '',
+                        children: [
+                          new TextSpan(
+                            text: 'Song Title\n',
+                            style: new TextStyle(
+                              color: Colors.white,
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 4.0,
+                              height: 1.5,
                             ),
-                            new Expanded(child: new Container()),
-                            new RawMaterialButton(
-                                shape: new CircleBorder(),
-                                fillColor: Colors.white,
+                          ),
+                          new TextSpan(
+                            text: 'Artist Name',
+                            style: new TextStyle(
+                              color: Colors.white.withOpacity(0.75),
+                              fontSize: 12.0,
+                              letterSpacing: 3.0,
+                              height: 1.5,
+                            ),
+                          )
+                        ],
+                      )),
+                      new Padding(
+                          padding: const EdgeInsets.only(top: 40.0),
+                          child: new Row(
+                            children: <Widget>[
+                              new Expanded(child: new Container()),
+                              new IconButton(
                                 splashColor: lightAccentColor,
-                                highlightColor:
-                                    lightAccentColor.withOpacity(0.5),
-                                elevation: 10.0,
-                                highlightElevation: 5.0,
+                                highlightColor: Colors.transparent,
+                                icon: new Icon(
+                                  Icons.skip_previous,
+                                  color: Colors.white,
+                                  size: 35.0,
+                                ),
                                 onPressed: () {
                                   // TODO
                                 },
-                                child: new Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: new Icon(
-                                    Icons.play_arrow,
-                                    color: darkAccentColor,
-                                    size: 35.0,
-                                  ),
-                                )),
-                            new Expanded(child: new Container()),
-                            new IconButton(
-                              icon: new Icon(
-                                Icons.skip_next,
-                                color: Colors.white,
-                                size: 35.0,
                               ),
-                              onPressed: () {
-                                // TODO
-                              },
-                            ),
-                            new Expanded(child: new Container()),
-                          ],
-                        )),
-                  ],
+                              new Expanded(child: new Container()),
+                              new RawMaterialButton(
+                                  shape: new CircleBorder(),
+                                  fillColor: Colors.white,
+                                  splashColor: lightAccentColor,
+                                  highlightColor:
+                                      lightAccentColor.withOpacity(0.5),
+                                  elevation: 10.0,
+                                  highlightElevation: 5.0,
+                                  onPressed: () {
+                                    // TODO
+                                  },
+                                  child: new Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: new Icon(
+                                      Icons.play_arrow,
+                                      color: darkAccentColor,
+                                      size: 35.0,
+                                    ),
+                                  )),
+                              new Expanded(child: new Container()),
+                              new IconButton(
+                                splashColor: lightAccentColor,
+                                highlightColor: Colors.transparent,
+                                icon: new Icon(
+                                  Icons.skip_next,
+                                  color: Colors.white,
+                                  size: 35.0,
+                                ),
+                                onPressed: () {
+                                  // TODO
+                                },
+                              ),
+                              new Expanded(child: new Container()),
+                            ],
+                          )),
+                    ],
+                  ),
                 ),
               )),
         ],
